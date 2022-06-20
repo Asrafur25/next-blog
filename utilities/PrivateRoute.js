8cda598415b35c98cab5b7c58bfa8eb4316399b8
istiak227 <istiak.ahmed227@gmail.com>
2022-06-20 21:44:02 +0600

    redirect route

--- utilities/PrivateRoute.js
+++ utilities/PrivateRoute.js
@@ -1,22 +1,44 @@
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect, useMemo } from "react";
 import useAuth from "../hooks/useAuth";
 import useFirebase from "../hooks/useFirebase";
+import Login from "../pages/login";
 
 const PrivateRoute = ({ children }) => {
-  const { user } = useAuth();
+  const { user, isLoading } = useAuth();
   const router = useRouter();
 
-  console.log("private user", user)
+  console.log("private user", user);
 
-  if (!user) {
-    console.log("going to loginssssssssssssssssss")
-    router.push("/login");
-  } else
+  useEffect(() => {
+    if (!isLoading) {
+      //auth is initialized and there is no user
+      if (!user.email) {
+        // remember the page that user tried to access
+        //setRedirect(router.route);
+        // redirect
+        router.push("/login");
+      }
+    }
+  }, [user]);
 
-  return <>{user ? children : null}</>;
+  // if(!user) {
+  //   console.log("going to loginssssssssssssssssss")
+  //   router.push("/login");
+  // }
+
+  // return <>{user ? children : null}</>;
+  if (isLoading) {
+    return <h1>Application Loading</h1>;
+  }
+
+  if (user) {
+    return <>{children}</>;
+  }
+
+  return null;
+
+  // If user is logged in, return original component
 };
-
-
 
 export default PrivateRoute;
